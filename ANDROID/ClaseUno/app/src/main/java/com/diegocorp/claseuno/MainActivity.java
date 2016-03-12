5c4f654a178f68edb1a31b0ef9b9e839f9b2cfda
Diego Armando Montoya Lopera <dmonto20@eafit.edu.co>
2016-03-12 11:26:36 -0500

    Avances

--- ANDROID/ClaseUno/app/src/main/java/com/diegocorp/claseuno/MainActivity.java
+++ ANDROID/ClaseUno/app/src/main/java/com/diegocorp/claseuno/MainActivity.java
@@ -1,15 +1,20 @@
+
+
 package com.diegocorp.claseuno;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+    //private FragmentTabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,36 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TabHost tabs=(TabHost)findViewById(R.id.contenedor_tabs );
+        tabs.setup();
+        TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
+        spec.setContent(R.id.linearLayout);
+        //spec.setContent( R.layout.fragment_viajes_actuales);
+        spec.setIndicator("Tab 1");
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.linearLayout2);
+        spec.setIndicator("TAB2");
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+        /*TabHost tabHost = (TabHost) findViewById( R.id.contenedor_tabs );
+
+        tabHost.setup();
+
+        TabHost.TabSpec tab = tabHost.newTabSpec("TAb 1");
+        tab.setIndicator("Indicador 1");
+        tab.setContent( R.id.linearLayout);
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("TAb 2");
+        tab1.setIndicator("Indicador 2");
+        tab.setContent(R.id.linearLayout2);
+
+        tabHost.addTab( tab );
+        tabHost.addTab( tab1 );*/
+
+
     }
 
     @Override
